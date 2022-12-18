2e185eec72fbb997c3637d5ee497c078dbf32864
Felix Thielen <felix.thielen@gmail.com>
2022-12-18 15:34:35 +0100

    fix broken link: Personen/ => Mitglieder/

--- codeigniter/app/Views/templates/navigation.php
+++ codeigniter/app/Views/templates/navigation.php
@@ -4,5 +4,5 @@
     <li class="list-group-item"><a href="Todos/">Aktuelles Projekt</a></li>
     <li class="list-group-item ms-4"><a href="Reiter">Reiter</a></li>
     <li class="list-group-item ms-4"><a href="Aufgaben/">Aufgaben</a></li>
-    <li class="list-group-item ms-4"><a href="Personen/">Mitglieder</a></li>
+    <li class="list-group-item ms-4"><a href="Mitglieder/">Mitglieder</a></li>
 </ul>
