e57af57c9c12c46fe4077127f080858ee151155c
Rob <117313375+RobtopHBY@users.noreply.github.com>
2023-01-23 17:30:07 +0100

    Login view
    login view überarbeitet

--- codeigniter/app/Views/login.php
+++ codeigniter/app/Views/login.php
@@ -6,15 +6,31 @@
             <div class="card-body">
                 <div class="form-group pb-2">
                     <label for="username">Benutzername:</label>
-                    <input type="text" class="form-control" id="username" name="username"/>
+                    <input type="text" class="form-control <?= (isset($error['username'])) ? 'is-invalid' : '' ?>" id="username"
+                            name="username"/>
+                    <div class="invalid-feedback">
+                        <?= $error['username'] ?? '' ?>
+                    </div>
                 </div>
                 <div class="form-group pb-2">
                     <label for="password">Passwort:</label>
-                    <input type="password" class="form-control" id="password" name="password"/>
+                    <input type="password" class="form-control <?= (isset($error['password'])) ? 'is-invalid' : '' ?>" id="password" name="password"/>
+                    <div class="invalid-feedback">
+                        <?= $error['password'] ?? '' ?>
+                    </div>
+                </div>
+
+            </div>
+            <div class="form-check">
+                <input name="AGB" class="form-check-input <?= (isset($error['AGB'])) ? 'is-invalid' : '' ?>" type="checkbox" value="1"  id="AGB-Box">
+                <label class="form-check-label" for="AGB-Box">
+                    AGB's akzeptieren
+                </label>
+                <div class="invalid-feedback">
+                    <?= $error['AGB'] ?? '' ?>
                 </div>
             </div>
             <button id="btnsubmit" type="submit" class="btn btn-primary">Einloggen</button>
-            </form>
             <p>Noch nicht registriert? <a href="" aria-label="Registrierung">Registrierung</a></p>
         </div>
     </div>
