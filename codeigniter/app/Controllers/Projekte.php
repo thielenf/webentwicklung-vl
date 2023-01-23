560414147a74b66b0796d40fe5fe1a53f9140a55
Rob <117313375+RobtopHBY@users.noreply.github.com>
2023-01-23 19:56:01 +0100

    Insert methode
    Neues projekt kann erstellt werden und wird in Datenbank eingefügt

--- codeigniter/app/Controllers/Projekte.php
+++ codeigniter/app/Controllers/Projekte.php
@@ -2,8 +2,30 @@
 
 namespace App\Controllers;
 
-class Projekte extends BaseController
-{
+use CodeIgniter\Model;
+use Config\App;
+
+class Projekte extends BaseController {
+    public function create() {
+        $projektname = $this->request->GetPost('Projektname');
+        $projektbeschreibung = $this->request->GetPost('Projektbeschreibung');
+       //$this->ProjekteModel->createProject('id', 'description');
+       $ProjekteModel = new \App\Models\ProjekteModel();
+
+       $ProjekteModel->createProject($projektname, $projektbeschreibung);
+
+        echo view('templates/header');
+        echo view('Projekte/Projekte');
+        echo view('templates/footer');
+
+
+
+    }
+    public function update() {
+        $this->load->model('ProjekteModel');
+        $this->ProjekteModel->updateProject(1, 'New Project Name', 'New Project Description');
+    }
+
     public function index()
     {
         echo view('templates/header');
